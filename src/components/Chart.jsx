f9eba95b9850923d063fb47e3b56195483d66682
Om Preetham <ompreethambandi@gmail.com>
2024-04-14 15:34:48 -0500

    ready to deploy

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,18 +1,31 @@
 import data from '../data/data.json'
 
 const Chart = () => {
+  // Find the index of the top spending item
+  const topIndex = data.findIndex(
+    (item) => item.amount === Math.max(...data.map((item) => item.amount))
+  )
+
   return (
     <div className="chart">
-      <p className="chart__title">Spending - Last 7 days</p>
+      <p className="chart__title">Spending &minus; Last 7 days</p>
       <div className="chart__graph-wrapper">
-        {data.map((item, index) => {
-          return (
-            <div key={index} className="chart__bar-wrapper">
-              <div className="chart__bar"></div>
-              <p className="chart__day">{item.day}</p>
-            </div>
-          )
-        })}
+        {data.map((item, index) => (
+          <div key={index} className="chart__bar-wrapper">
+            <div className="chart__bar-amount">${item.amount}</div>
+            <div
+              className="chart__bar"
+              style={{
+                height: `${item.amount / 6}rem`,
+                backgroundColor:
+                  index === topIndex
+                    ? 'hsl(186, 34%, 60%)'
+                    : 'hsl(10, 79%, 65%)', // Use topIndex to determine color
+              }}
+            ></div>
+            <p className="chart__day">{item.day}</p>
+          </div>
+        ))}
       </div>
       <div className="chart__stats-wrapper">
         <div className="chart__month-wrapper">
@@ -27,4 +40,5 @@ const Chart = () => {
     </div>
   )
 }
+
 export default Chart
